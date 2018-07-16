5cad6bb095a1b49a5a182dcb6d43480d7d032b81
Viktor Gars <viktor@vgsoftware.com>
2018-07-16 14:11:36 +0200

    tst commit

---
+++ My Test Project 2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace My_Test_Project_2
+{
+	public class Class1
+	{
+		public Class1()
+		{
+			var kalle = new Nansen.Encore.EditIcons.ContentIcon();
+		}
+	}
+}
